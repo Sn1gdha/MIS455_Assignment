3514bb363d4ebc48979414ed7753927fec2067e6
Sn1gdha <sunzidasnigdha@gmail.com>
2024-10-24 03:16:59 +0600

    added file

---
+++ page2.js
@@ -0,0 +1,5 @@
+var StudentName=prompt("Enter your name?");
+var quiz1=parseInt(prompt("Enter your quiz1 mark"));
+var quiz2=parseInt(prompt("Enter your quiz2 mark"));
+var total=quiz1+quiz2;
+console.log("total marks",total);
