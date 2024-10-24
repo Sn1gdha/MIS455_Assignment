b8c3d7cd7f07355ed7e8ee445c8ad93ecaac7e52
Sn1gdha <sunzidasnigdha@gmail.com>
2024-10-24 11:31:47 +0600

    main page

--- page2.js
+++
@@ -1,5 +0,0 @@
-var StudentName=prompt("Enter your name?");
-var quiz1=parseInt(prompt("Enter your quiz1 mark"));
-var quiz2=parseInt(prompt("Enter your quiz2 mark"));
-var total=quiz1+quiz2;
-console.log("total marks",total);
